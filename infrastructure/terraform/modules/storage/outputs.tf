516fc91b25aa3a3c1e370b3725d3867fbf9a6444
Dakota Harris <dakota.harris@fullfact.org>
2025-07-22 11:02:00 +0100

    chore: remove accidentally added infrastructure

--- infrastructure/terraform/modules/storage/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "bucket_name" {
-  value = google_storage_bucket.storage_bucket.name
-}
-
-output "bucket_uri" {
-  # lifehack
-  value = "gs://${google_storage_bucket.storage_bucket.name}"
-}
