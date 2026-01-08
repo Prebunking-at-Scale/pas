83bb59931008381133025a3b18b4558a9d926cdf
James McMinn <james.mcminn@fullfact.org>
2026-01-08 22:09:58 +0000

    feat: first pass at instagram scraper

---
+++ projects/src/instascraper/instascraper/scrape.py
@@ -0,0 +1,49 @@
+from os import path
+from uuid import UUID
+
+from instascraper.storage import DiskStorageClient, StorageClient
+import structlog
+from instascraper import coreapi, instagram
+
+logger: structlog.BoundLogger = structlog.get_logger(__name__)
+
+
+def scrape_channel(
+    channel: str, cursor: str | None, storage_client: StorageClient, org_ids: list[UUID]
+):
+    log = logger.new(cursor=cursor, channel=channel)
+
+    next_cursor = None
+    profile = instagram.fetch_profile(channel)
+    reels = profile.reels
+    log.debug(f"got {len(reels)} reels for user")
+    for reel in reels:
+        try:
+            if reel.id == cursor:
+                break
+
+            if not next_cursor:
+                # The first video will be the most recent
+                next_cursor = reel.id
+
+            bytes = reel.video_bytes()
+            blob_name = path.join(channel, f"{reel.id}.mp4")
+            blob_path = storage_client.upload_blob(blob_name, bytes)
+            coreapi.register_download(reel, org_ids, blob_path)
+
+        except Exception as ex:
+            log.error(
+                "exception with downloading, skipping entry",
+                event_metric="download_failure",
+                exc_info=ex,
+            )
+
+    return next_cursor
+
+
+if __name__ == "__main__":
+    storage_client = DiskStorageClient("./reels/")
+    next_cursor = scrape_channel(
+        "alimasadia_", "3364843860104643554", storage_client, []
+    )
+    print(next_cursor)
---
+++ projects/src/instascraper/instascraper/storage.py
@@ -0,0 +1,48 @@
+import io
+from os import path
+from pathlib import Path
+from typing import Protocol
+import structlog
+from google.cloud import storage
+
+logger: structlog.BoundLogger = structlog.get_logger(__name__)
+
+
+class StorageClient(Protocol):
+    def upload_blob(
+        self, blob_name: str, buf: io.BytesIO, content_type: str = "video/mp4"
+    ) -> str: ...
+
+
+class DiskStorageClient:
+    """Store blobs locally. This is intended mostly for testing purposes."""
+
+    def __init__(self, folder: str):
+        self.folder = folder
+
+    def upload_blob(
+        self, blob_name: str, buf: io.BytesIO, content_type: str = ""
+    ) -> str:
+        blob_path = path.join(self.folder, blob_name)
+        Path(blob_path).parent.mkdir(parents=True, exist_ok=True)
+        with open(blob_path, "wb") as f:
+            f.write(buf.getbuffer())
+        return blob_path
+
+
+class GoogleCloudStorageClient:
+    def __init__(self, bucket_name: str, path_prefix: str):
+        self.bucket_name = bucket_name
+        self.path_prefix = path_prefix
+        self.client = storage.Client()
+        self.bucket = storage.Bucket(bucket_name)
+
+    def upload_blob(
+        self, blob_name: str, buf: io.BytesIO, content_type: str = "video/mp4"
+    ) -> str:
+        blob_path = path.join(self.path_prefix, blob_name)
+        log = logger.bind(blob_path=blob_path)
+        log.debug(f"uploading blob to path {blob_path}")
+        blob = self.bucket.blob(blob_path)
+        blob.upload_from_file(buf, content_type=content_type)
+        return blob_path
